f8bb93d201c1f2fbdffc2f775af6b35deb890707
Pruthviraj Patel <pruthviraj_patel@sfu.ca>
2022-02-08 16:39:39 -0800

    Added a sample form to make a post call for transforming data

--- src/main/java/com/assignment/markovchaindemo/controllers/FileContentTransformationController.java
+++ src/main/java/com/assignment/markovchaindemo/controllers/FileContentTransformationController.java
@@ -2,12 +2,13 @@ package com.assignment.markovchaindemo.controllers;
 
 import com.assignment.markovchaindemo.models.InputData;
 import com.assignment.markovchaindemo.services.FileContentTransformationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 public class FileContentTransformationController {
     private final FileContentTransformationService fileContentTransformationService;
 
@@ -15,21 +16,28 @@ public class FileContentTransformationController {
         this.fileContentTransformationService = fileContentTransformationService;
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+
     /*
             I tried using @ModelAttribute with a POJO that I created in models folder, but that didn't work since
             it didn't have an @Id or @Entity annotations and I believe @ModelAttribute only works if the model
             has @Entity annotation with its id being annotated with @Id
      */
-    @PostMapping("/")
+    @PostMapping("/upload")
     public String index(@RequestParam MultipartFile file,
                         @RequestParam int order,
                         @RequestParam String outputFilePath) {
+
+
         // Todo: remove InputData???
         InputData data = new InputData(file, order, outputFilePath);
 
         String fileContent = fileContentTransformationService.readFileContent(file);
         fileContentTransformationService.writeToFile(fileContent, outputFilePath);
 
-        return "successfully applied markov chain algorithm and the output file has been created";
+        return "success";
     }
 }
